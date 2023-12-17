0ba38948109a68c4a8e4815218b0b51e6fe2f098
Alexander Satretdinov <alexander.satretdinov@webpros.com>
2023-12-17 21:00:08 +0200

    get rid of debug statement

--- apps/backend-api/src/auth/guards/auth.guard.ts
+++ apps/backend-api/src/auth/guards/auth.guard.ts
@@ -5,8 +5,6 @@ export class AuthGuard implements CanActivate {
   public canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
 
-    console.log('request user', request.user);
-
     return !!request.user;
   }
 }
