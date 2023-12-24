89b4656d22892eb8e0b4a2b9ca7abe5a682b230f
Alexander Satretdinov <alexander.satretdinov@webpros.com>
2023-12-25 00:43:16 +0200

    implement login page

--- apps/frontend-admin/src/components/Auth/Login.tsx
+++ apps/frontend-admin/src/components/Auth/Login.tsx
@@ -1,7 +1,35 @@
+import { Button } from '@app/frontend-admin/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@app/frontend-admin/components/ui/card';
+
 export function Login() {
   return (
-    <main className="container">
-      <a href="/api/auth/login">Login</a>
-    </main>
+    <div>
+      <div className="flex items-center justify-center h-[100vh]">
+        <Card>
+          <CardHeader>
+            <CardTitle>{'Welcome to Dudes Admin Panel'}</CardTitle>
+            <CardDescription>
+              {'The way to manage your stream dudes.'}
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Button
+              className="w-full bg-[#9147ff] text-white hover:bg-[#772ce8]"
+              asChild
+            >
+              <a href="/api/auth/login" target="_blank">
+                {'Twitch Login'}
+              </a>
+            </Button>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
   );
 }
--- apps/frontend-admin/src/main.tsx
+++ apps/frontend-admin/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-// @TODO: uncomment when new UI will be implemented.
-// import './style.css';
+import './style.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
